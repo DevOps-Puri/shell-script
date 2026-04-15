9cf9433c9f0fafe6beaa0d444a9dbf16e7864c49
purusreddy <c.nagapurushothamreddy@gmail.com>
2026-04-15 11:12:53 +0530

    Variable script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,4 +4,10 @@ echo "please enter your username: "
 
 read -s USERNAME
 
-echo "Hi $USERNAME, welcome to DevOps Puri"
+echo "username entered $USERNAME "
+
+echo "please enter your password: "
+
+read -s PASSWORD
+
+echo "password entered $PASSWORD "
