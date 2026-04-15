b1484939fda48da833035aceb41e0457e5b924cf
purusreddy <c.nagapurushothamreddy@gmail.com>
2026-04-15 12:27:10 +0530

    Variable script added

---
+++ 08-special-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+All variables passed: $@
+Number of variables: $#
+script name: $0
+present working directory: $PWD
+home directory of current user: $HOME
+Which user is running the script: $USER
+process id of current script: $$
+process id of last command in background : $!
