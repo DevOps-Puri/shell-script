1a754d8dd18204a0f1204518a7a3087a655a8a30
purusreddy <c.nagapurushothamreddy@gmail.com>
2026-04-15 12:20:19 +0530

    Variable script added

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,8 +3,8 @@
 MOVIES=("pushpa" "rrr" "devara")
 # index starts from 0 and size is 3
 
-echo" first movie is ${MOVIES[0]}"
-echo" second movie is ${MOVIES[1]}"
-echo" third movie is ${MOVIES[2]}"
+echo " first movie is ${MOVIES[0]}"
+echo " second movie is ${MOVIES[1]}"
+echo " third movie is ${MOVIES[2]}"
 
 echo "all movies are ${MOVIES[@]}"
