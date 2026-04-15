a21804dce43c803634804451a9b8934cee719dd2
purusreddy <c.nagapurushothamreddy@gmail.com>
2026-04-15 15:20:01 +0530

    Variable script added

---
+++ 09-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER1=$1
+
+# -gt ,-lt,-eq,-ge,-le
+
+if [ SNUMBER1 -gt 100]
+then
+    echo "Given number is greater than 100"
+else
+    echo "Given number is less than or equal to 100"
+fi
