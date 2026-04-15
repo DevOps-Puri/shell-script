4d3ed910b6b82b2eb56bad3dc830de18ba624305
purusreddy <c.nagapurushothamreddy@gmail.com>
2026-04-15 11:02:24 +0530

    Variable script added

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 #no space allowed between variable name and value
+PERSON2=$2 #no space allowed between variable name and value
+
+echo "$PERSON1:: Hi $PERSON2 "
+echo "$PERSON2:: Hello $PERSON1 "
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am doing good, how are you?"
